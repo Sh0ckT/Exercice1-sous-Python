2d2359997e40c646f3b8ff89a6316e5eb88ed184
Sh0ckT <90909498+Sh0ckT@users.noreply.github.com>
2021-10-11 20:25:56 +0200

    Create Ex1.py

---
+++ Ex1.py
@@ -0,0 +1,3 @@
+a = int(input("Entrez la valeur de A: "))#permet d'entrer une valeur pour A
+b = int(input("Entrez la valeur de B: "))#permet d'entrer une valeur pour B
+print("Le résultat de la multiplication est de " + str(a*b)) #donne le résultat de la multiplication
